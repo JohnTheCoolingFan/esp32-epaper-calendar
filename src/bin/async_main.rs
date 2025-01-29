d016233f9d502e8bc5979493ef260a9192ddb4ed
JohnTheCoolingFan <ivan8215145640@gmail.com>
2025-01-29 18:02:47 +0300

    Cleanup

--- src/bin/async_main.rs
+++ src/bin/async_main.rs
@@ -1,8 +1,6 @@
 #![no_std]
 #![no_main]
 
-use core::cell::RefCell;
-
 use display_interface_spi::SPIInterface;
 use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
 use embassy_executor::Spawner;
@@ -10,24 +8,21 @@ use embassy_sync::blocking_mutex::raw::NoopRawMutex;
 use embassy_time::{Duration, Timer};
 use embedded_graphics::{
     mono_font::MonoTextStyle,
-    prelude::{Point, Primitive},
-    primitives::{Line, PrimitiveStyle},
+    prelude::*,
     text::{Text, TextStyle},
     Drawable,
 };
-use embedded_hal_bus::spi::{ExclusiveDevice, RefCellDevice};
 use esp_backtrace as _;
 use esp_hal::{
     dma::{Dma, DmaPriority, DmaRxBuf, DmaTxBuf},
     dma_buffers,
     gpio::{Input, Level, NoPin, Output, Pull},
-    peripherals::SPI2,
     prelude::*,
     spi::{
         master::{Config, Spi, SpiDmaBus},
         SpiMode,
     },
-    Async, Blocking,
+    Async,
 };
 use log::info;
 
@@ -50,8 +45,7 @@ async fn main(spawner: Spawner) {
 
     esp_alloc::heap_allocator!(72 * 1024);
 
-    let mut delay = embassy_time::Delay;
-    //let mut delay = esp_hal::delay::Delay::new();
+    let delay = embassy_time::Delay;
 
     esp_println::logger::init_logger_from_env();
 
@@ -59,8 +53,8 @@ async fn main(spawner: Spawner) {
     esp_hal_embassy::init(timer0.timer0);
 
     info!("Embassy initialized!");
+    info!("WiFi init");
 
-    /*
     let timer1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
     let _init = esp_wifi::init(
         timer1.timer0,
@@ -68,7 +62,6 @@ async fn main(spawner: Spawner) {
         peripherals.RADIO_CLK,
     )
     .unwrap();
-    */
 
     info!("Initializing pins");
 
@@ -94,28 +87,20 @@ async fn main(spawner: Spawner) {
             ..Config::default()
         },
     )
-    //.with_cs(peripherals.GPIO5)
     .with_cs(NoPin)
     .with_miso(NoPin)
     .with_sck(peripherals.GPIO18)
     .with_mosi(peripherals.GPIO21)
     .with_dma(dma_channel.configure(false, DmaPriority::Priority0))
     .with_buffers(dma_rx_buf, dma_tx_buf);
-    //let spi_bus = RefCell::new(spi_bus);
     let spi_bus = embassy_sync::mutex::Mutex::<NoopRawMutex, _>::new(spi_bus);
 
     info!("Initializing spi device");
 
-    let mut spi_device = SpiDevice::new(&spi_bus, cs);
-    //let mut spi = RefCellDevice::new(&spi_bus, cs, delay).unwrap();
-    //let mut spi_device =
-    //    ExclusiveDevice::new(spi_bus, cs, delay).expect("SPI device initialization error");
-    let mut spi_interface = SPIInterface::new(spi_device, dc);
+    let spi_device = SpiDevice::new(&spi_bus, cs);
+    let spi_interface = SPIInterface::new(spi_device, dc);
 
     info!("Initializing epd");
-
-    //let mut epd =
-    //    Epd2in9bc::new(&mut spi, busy_in, dc, rst, &mut delay, None).expect("EPD creation error");
 
     let mut driver = WeActStudio290TriColorDriver::new(spi_interface, busy_in, rst, delay);
     driver.init().await.unwrap();
@@ -144,36 +129,10 @@ async fn main(spawner: Spawner) {
     )
     .draw(&mut display);
 
+    info!("Display full update");
+
     driver.full_update(&display).await.unwrap();
 
-    /*
-        let mut mono_display = Display2in9bc::default();
-        mono_display.set_rotation(DisplayRotation::Rotate90);
-
-        let _ = Line::new(Point::new(0, 120), Point::new(0, 200))
-            .into_styled(PrimitiveStyle::with_stroke(Color::Black, 1))
-            .draw(&mut mono_display);
-
-        let mut chromatic_display = Display2in9bc::default();
-        chromatic_display.set_rotation(DisplayRotation::Rotate90);
-
-        let _ = Line::new(Point::new(15, 120), Point::new(15, 200))
-            .into_styled(PrimitiveStyle::with_stroke(Color::Black, 1))
-            .draw(&mut chromatic_display);
-    */
-    info!("Updating color frame");
-
-    /*
-        epd.update_color_frame(
-            &mut spi,
-            &mut delay,
-            mono_display.buffer(),
-            chromatic_display.buffer(),
-        )
-        .unwrap();
-
-        epd.display_frame(&mut spi, &mut delay).unwrap();
-    */
     info!("Pre sleep");
 
     driver.sleep().await.unwrap();
