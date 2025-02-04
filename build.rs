2aec505067158bf5a915ce1fc918197eb48155b3
JohnTheCoolingFan <ivan8215145640@gmail.com>
2025-02-05 00:48:04 +0300

    Add wifi creds from file reading which is NOT IN THE REPO
    No need to specify at command line anymore and teh file won't be shared
    outside of the local project folder. I wish some paid project did this
    level of security...

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("cargo:rustc-link-arg-bins=-Tlinkall.x");
+    let creds_lines = std::fs::read_to_string("wifi-creds").unwrap();
+    for line in creds_lines.lines() {
+        if !line.is_empty() {
+            let val_pair = line.trim_start().trim_end();
+            println!("cargo:rustc-env={val_pair}")
+        }
+    }
 }
