34d129237557e962be1a9caa2f6b41e0c006d011
JohnTheCoolingFan <ivan8215145640@gmail.com>
2025-03-21 22:04:54 +0300

    Move drawing to a folder module

--- src/bin/async_main/draw/mod.rs
+++ src/bin/async_main/draw/mod.rs
@@ -2,9 +2,12 @@ use chrono::DateTime;
 use chrono_tz::Tz;
 use embedded_graphics::prelude::DrawTarget;
 
+use crate::calendar_utils::CalendarMonth;
+
 pub async fn draw_calendar<D: DrawTarget>(
     time: &DateTime<Tz>,
     display: &mut D,
 ) -> Result<(), D::Error> {
+    let calendar = CalendarMonth::from_date(time.naive_local().date());
     todo!()
 }
