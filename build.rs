4c9713f6ba6558b37d577fea1709dd353e0b3c78
JohnTheCoolingFan <ivan8215145640@gmail.com>
2026-05-21 23:50:50 +0300

    don't mandate wifi-creds file

--- build.rs
+++ build.rs
@@ -2,11 +2,13 @@ fn main() {
     println!("cargo:rustc-link-arg-bins=-Tlinkall.x");
     #[cfg(feature = "networking")]
     {
-        let creds_lines = std::fs::read_to_string("wifi-creds").unwrap();
-        for line in creds_lines.lines() {
-            if !line.is_empty() {
-                let val_pair = line.trim_start().trim_end();
-                println!("cargo:rustc-env={val_pair}")
+        // Don't unwrap if file doesn't exist so the user can just set env vars at build time
+        if let Ok(creds_lines) = std::fs::read_to_string("wifi-creds") {
+            for line in creds_lines.lines() {
+                if !line.is_empty() {
+                    let val_pair = line.trim_start().trim_end();
+                    println!("cargo:rustc-env={val_pair}")
+                }
             }
         }
     }
