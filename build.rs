5fd3d7bbee96cd83e2025169db0832897ba5fefe
JohnTheCoolingFan <ivan8215145640@gmail.com>
2026-05-21 23:49:31 +0300

    don't look for wifi creds if networking is disabled

--- build.rs
+++ build.rs
@@ -1,10 +1,13 @@
 fn main() {
     println!("cargo:rustc-link-arg-bins=-Tlinkall.x");
-    let creds_lines = std::fs::read_to_string("wifi-creds").unwrap();
-    for line in creds_lines.lines() {
-        if !line.is_empty() {
-            let val_pair = line.trim_start().trim_end();
-            println!("cargo:rustc-env={val_pair}")
+    #[cfg(feature = "networking")]
+    {
+        let creds_lines = std::fs::read_to_string("wifi-creds").unwrap();
+        for line in creds_lines.lines() {
+            if !line.is_empty() {
+                let val_pair = line.trim_start().trim_end();
+                println!("cargo:rustc-env={val_pair}")
+            }
         }
     }
 }
